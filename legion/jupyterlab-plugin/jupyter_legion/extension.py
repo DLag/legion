c1aede53afd38c0db0c331bcddb7cd93d5325ccf
Kirill Makhonin <kroks.rus@gmail.com>
2019-05-24 14:40:50 +0300

    [#895] Update copyrights

--- legion/jupyterlab-plugin/jupyter_legion/extension.py
+++ legion/jupyterlab-plugin/jupyter_legion/extension.py
@@ -1,9 +1,19 @@
-from notebook.base.handlers import APIHandler
-from notebook.utils import url_path_join, to_os_path, exists, to_api_path
-from tornado import web
-from slugify import slugify
-from contextlib import contextmanager
-from tornado.escape import json_decode
+#
+#    Copyright 2019 EPAM Systems
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+from notebook.utils import url_path_join
 
 from .api_state import ApiState
 from .handlers import register_all_handlers
